c1153a7698a2493d6366f21d8d9d37180475a55a
Jeremy Dowell <jdowell@fossil.com>
2013-05-10 15:00:49 -0500

    Better Testing

--- examples/test.js
+++ examples/test.js
@@ -1,14 +1,27 @@
 var pHash = require('../index');
 
 var hashA = pHash.getImageHash("a.jpg");
+console.log("HashA: " + hashA);
+
 var hashB = pHash.getImageHash("b.png");
+console.log("HashB: " + hashB);
+
 var hashC = pHash.getImageHash("c.png");
+console.log("HashC: " + hashC);
+
 var hashD = pHash.getImageHash("d.jpg");
+console.log("HashD: " + hashD);
+
 
 //not really an image attempting to create an exception
 var hashF = pHash.getImageHash("f.png");
+console.log("not an image: " + hashF);
+
 var hashG = pHash.getImageHash("g.jpg");
+console.log("nonRBG image: " + hashG);
+
 var hashH = pHash.getImageHash("h.jpg");
+console.log("empty image: " + hashH);
 
 
 var hammingAB = pHash.hammingDistance(hashA,hashB);
@@ -16,13 +29,6 @@ var hammingAC = pHash.hammingDistance(hashA,hashC);
 var hammingBC = pHash.hammingDistance(hashB,hashC);
 var hammingAD = pHash.hammingDistance(hashA,hashD);
 
-console.log("HashA: " + hashA);
-console.log("HashB: " + hashB);
-console.log("HashC: " + hashC);
-console.log("HashD: " + hashD);
-console.log("not an image: " + hashF);
-console.log("nonRBG image: " + hashG);
-console.log("empty image: " + hashH);
 
 console.log("Hamming Distance A -> B: " + hammingAB);
 console.log("Hamming Distance A -> C: " + hammingAC);
